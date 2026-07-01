2dd357338ca02008d9f09f110f2f078323c84bff
kadiwalaaman1288 <kadiwalaaman57@gmail.com>
2026-07-01 12:01:47 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -19,7 +19,7 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "https://multi-vendor-ecommerce-sooty.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
